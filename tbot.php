95dd68e21b61dcf0a811beb662172241921406bc
Рóман <38821738+roakozlov@users.noreply.github.com>
2020-06-05 01:11:34 +0300

    Add url variable

--- tbot.php
+++ tbot.php
@@ -1,20 +1,20 @@
 <?php 
 
 function bot_start($param){
-	global $token;
-	$token = $param;
-	return json_decode(file_get_contents('php://input'));
+  global $token;
+  $token = $param;
+  return json_decode(file_get_contents('php://input'));
 }
 
 function bot($method, $params){
-	global $token, $url;
-	if (!$url){
-		$url = 'https://api.telegram.org';
-	}
-	if ($params){
-		$result = file_get_contents($url.'/bot'.$token.'/'.$method.'?'.http_build_query($params));
-	}else{
-		$result = file_get_contents($url.'/bot'.$token.'/'.$method);
-	}
-	return $result;
+  global $token, $url;
+  if (!$url){
+    $url = 'https://api.telegram.org';
+  }
+  if ($params){
+    $result = file_get_contents($url.'/bot'.$token.'/'.$method.'?'.http_build_query($params));
+  }else{
+    $result = file_get_contents($url.'/bot'.$token.'/'.$method);
+  }
+  return $result;
 }
