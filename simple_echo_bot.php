2c730d80ea7583bb719df1235fcc960ecfa171a7
Рóман <38821738+roakozlov@users.noreply.github.com>
2020-06-05 01:10:21 +0300

    Add bot example

--- simple_echo_bot.php
+++ simple_echo_bot.php
@@ -4,11 +4,11 @@ require_once('tbot.php');
 $bot = bot_start('1234567890:QwErTyUiOpAsDfGhJkLzXcVbNmQwErTyU');
 
 switch ($bot->message->text) {
-	case '/start':
-			bot('sendMessage', ['chat_id' => $bot->message->chat->id, 'parse_mode' => 'HTML', 'text' => 'I work in <a href="https://github.com/roakozlov/tbot.php">tbot.php</a> library by <a href="https://roakozlov.ml/">roakozlov</a>']);
-		break;
+  case '/start':
+    bot('sendMessage', ['chat_id' => $bot->message->chat->id, 'parse_mode' => 'HTML', 'text' => 'I work in <a href="https://github.com/roakozlov/tbot.php">tbot.php</a> library by <a href="https://roakozlov.ml/">roakozlov</a>']);
+  break;
 	
-	default:
-			bot('sendMessage', ['chat_id' => $bot->message->chat->id, 'text' => $bot->message->text]);
-		break;
+  default:
+    bot('sendMessage', ['chat_id' => $bot->message->chat->id, 'text' => $bot->message->text]);
+  break;
 }
